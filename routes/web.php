9d06b49f0c0c1a213e259be76712d48388668ba6
olivier <olivier.himblot@le-campus-numerique.fr>
2023-06-12 09:23:39 +0200

    etape 2.1

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Homepage');
+});
+
+Route::get('/product', function () {
+    return view('Liste des produits');
+});
+
+Route::get('/product/{id}', function () {
+    return view('Fiche du produit {id}');
+});
+
+Route::get('/cart', function () {
+    return view('Panier');
 });
